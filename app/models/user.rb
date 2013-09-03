3b4a04682377c44541a15f5028a21b6d0203f74e
App Academy Student <student@appacademy.io>
2013-09-02 20:49:25 -0700

    added email uniqueness to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,27 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   attr_accessible :bio, :home_id, :password_hash, :selfie, :session_token, :username, :email, :password
   attr_reader :password
-  
+
   include BCrypt
   before_validation { self.reset_session_token! if self.session_token.nil? }
   validate :password_hash, :session_token, :username, presence: true
   validate :password, :length => { minimum: 4 }
+  validate :username, :email, uniqueness: true
 
   has_one :home
   has_many :items, through: :home, source: :items
   has_many :rented_items, through: :home, source: :items
   has_many :rentals, class_name: "Rental", foreign_key: :user_id
   has_many :reviews, class_name: "RentalReview", foreign_key: :author_id
-  
+
 
   def authenticate(password)
     BCrypt::Password.new(self.password_hash) == password
+  end
+
+  def email=(email)
+    #standardize input for uniqueness test
+    super(email.downcase.strip)
   end
 
   # is this necessary?
