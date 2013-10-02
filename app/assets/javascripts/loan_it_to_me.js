bf37f4477b80fa4079f87192294136bad2f6200f
Nate Tate <nate91711@gmail.com>
2013-10-01 20:52:11 -0700

    cleaning up comments

--- app/assets/javascripts/loan_it_to_me.js
+++ app/assets/javascripts/loan_it_to_me.js
@@ -4,15 +4,7 @@ window.LoanItToMe = {
   Views: {},
   Routers: {},
   initialize: function(options) {
-    //var options = { $rootEl: $('.main-content') };
     LoanItToMe.mainRouter = new LoanItToMe.Routers.Main( options );
     Backbone.history.start({ pushState: true, hashChange: false });
   }
 };
-
-//moved the initial on-ready function into every view that kicks off the app
-//because the app has more than one entry point
-// $(document).ready(function() {
-//   var options = { $rootEl: $('.main-content') };
-//   LoanItToMe.initialize(options);
-// });
